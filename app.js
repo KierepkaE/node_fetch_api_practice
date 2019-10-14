443be32b3fdbcc288b8de5a1e4f0f2d3ce3358d0
kierepkae <kierepkaedyta@gmail.com>
2019-10-14 16:03:22 +0100

    user can see text for particular random eyar

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const year = process.argv[2];
 
 fetch(url)
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => console.log(data.text))
   .catch(err => console.log('error:', err))
